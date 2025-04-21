d1ac56ecb222a6023f886e9592986878ec43985e
Cesar Caballero <udemicscesar@gmail.com>
2025-04-21 18:37:48 -0400

    añadir clase escenario

---
+++ PrimerFigura/Escenario.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK.Mathematics;
+
+namespace PrimerFigura
+{
+    class Escenario
+    {
+        private Vector3 posicion;
+        private Dictionary<string, Objeto> Objetos;
+
+        public Escenario()
+        {
+            this.posicion = new Vector3(0.0f, 0.0f, 0.0f);
+            this.Objetos = [];
+        }
+
+        public Escenario(Vector3 posicion)
+        {
+            this.posicion = posicion;
+            this.Objetos = [];
+        }
+
+        public void SetDiccionaryObjetos(Objeto[] ObjetosArray)
+        {
+            int i = 1;
+            foreach (Objeto objeto in ObjetosArray)
+            {
+                this.Objetos.Add("Objeto" + i, objeto);
+            }
+        }
+
+        public void dibujar(Shader shader)
+        {
+            foreach (var objeto in Objetos)
+            {
+                objeto.Value.dibujar(posicion, shader);
+            }
+        }
+    }
+}
